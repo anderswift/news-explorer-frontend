ed94583db9f02bcbcc21511f90e0b432799a98ea
Ander Swift <anderswift@gmail.com>
2021-03-13 17:16:43 -0500

    responsive style for search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,20 @@
+import './SearchForm.css';
+
 function SearchForm() {
   return (
-    <form>
-      hi form
+  
+    <form className="search">
+
+      <h2 className="search__heading">What's going on in the world?</h2>
+      <p className="search__desc">Find the latest news on any topic and save articles in your personal account.</p>
+      
+      <fieldset class="search__bar">
+        <input className="search__field" type="text" placeholder="Enter topic" />
+        <button className="search__button" type="submit">Search</button>
+      </fieldset>
+
     </form>
+
   );
 }
 
